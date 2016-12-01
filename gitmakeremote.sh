511fb7774a21556251830eb940a49c99b9e9e0dc
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-11-30 22:53:46 -0800

    better gitmakeremote

---
+++ gitmakeremote.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+response=$(curl -u 'jkopczyn' https://api.github.com/user/repos -d "{\"name\":\"$1\"}" -w "%{http_code}" --output "/dev/null";)
+printf "%s\n" "$response"
+if [ "201" == "$response" ]
+then
+    git init;
+    git remote add origin git@github.com:jkopczyn/$1.git;
+    echo "success!"
+fi
