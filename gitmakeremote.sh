aa14f3cb8dbd681ebf35faf525651b89d49e72c1
Jacob Kopczynski <ja.kopczynski@gmail.com>
2017-05-17 01:24:46 -0700

    defaults to dirname if not given a project name

--- gitmakeremote.sh
+++ gitmakeremote.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-response=$(curl -u 'jkopczyn' https://api.github.com/user/repos -d "{\"name\":\"$1\"}" -w "%{http_code}" --output "/dev/null";)
+response=$(curl -u 'jkopczyn' https://api.github.com/user/repos -d "{\"name\":\"${1:-${PWD##*/}}\"}" -w "%{http_code}" --output "/dev/null";)
 printf "%s\n" "$response"
 if [ "201" == "$response" ]
 then
     git init;
-    git remote add origin git@github.com:jkopczyn/$1.git;
+    git remote add origin git@github.com:jkopczyn/${1:-${PWD##*/}}.git;
     echo "success!"
 fi
